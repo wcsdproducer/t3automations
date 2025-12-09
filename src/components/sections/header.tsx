f68fc1e4be64e88554c9138cf79417de225f4bb0
John Freeman <john@t3kniq.com>
2025-12-09 14:17:12 +0000

    Make the background of the header on the Landing Page solid white

--- src/components/sections/header.tsx
+++ src/components/sections/header.tsx
@@ -20,7 +20,7 @@ export default function Header() {
   }, []);
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full border-b bg-white">
       <div className="container flex h-16 items-center">
         <div className="mr-6 flex items-center">
           <Link href="/" className="flex items-center space-x-2">
