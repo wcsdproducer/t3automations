67ce4829a84c7326515accb2e58bf0194216dc41
John Freeman <john@t3kniq.com>
2025-12-09 14:19:13 +0000

    Remove the dark overlay above the video in the hero section

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -17,7 +17,6 @@ export default function Hero() {
           allowFullScreen
         ></iframe>
       </div>
-      <div className="absolute inset-0 bg-black opacity-50 z-10"></div>
       <div className="container z-20 text-center">
         <div className="mx-auto max-w-3xl">
           <h1 className="text-4xl font-bold tracking-tight md:text-6xl">
