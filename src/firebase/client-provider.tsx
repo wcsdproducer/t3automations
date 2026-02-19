d78f88f2de8feabe53a01077f27731cacb94ee00
John Freeman <john@t3kniq.com>
2026-02-19 19:30:39 +0000

    Set up Storage for this app for media

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -20,6 +20,7 @@ export function FirebaseClientProvider({ children }: FirebaseClientProviderProps
       firebaseApp={firebaseServices.firebaseApp}
       auth={firebaseServices.auth}
       firestore={firebaseServices.firestore}
+      storage={firebaseServices.storage}
     >
       {children}
     </FirebaseProvider>
