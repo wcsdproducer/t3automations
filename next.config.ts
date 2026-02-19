fbdfd673fcf9a2542be4e05b2f4d436ea7f7ab5d
John Freeman <john@t3kniq.com>
2026-02-19 21:01:16 +0000

    on the Home Page, change the image for Steven Forester to: https://fireb

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -31,6 +32,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'storage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
         port: '',
         pathname: '/**',
       }
