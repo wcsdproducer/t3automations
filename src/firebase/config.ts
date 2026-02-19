378c474150d0b035f412f5abec03329e9ce4c381
John Freeman <john@t3kniq.com>
2026-02-19 19:43:50 +0000

    Add this storage to the app: gs://studio-1410114603-9e1f6.firebasestorag

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,8 +4,7 @@ export const firebaseConfig = {
   "appId": "1:68343157012:web:badd07358f2c0a46187201",
   "apiKey": "AIzaSyCbqIKV0sQGCwd3bZL65WnezElCE4J9-X8",
   "authDomain": "studio-1410114603-9e1f6.firebaseapp.com",
-  "storageBucket": "studio-1410114603-9e1f6.appspot.com",
+  "storageBucket": "studio-1410114603-9e1f6.firebasestorage.app",
   "measurementId": "",
   "messagingSenderId": "68343157012"
 };
-
