57f7ca73f2df5830e4430c39bf9258544ecc42c8
John Freeman <john@t3kniq.com>
2026-02-20 14:42:10 +0000

    Use the following image for the background of the hero section of the ho

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -2,13 +2,24 @@
 
 import { Button } from '@/components/ui/button';
 import { ChevronsDown } from 'lucide-react';
+import Image from 'next/image';
 
 export default function Hero() {
   return (
     <section className="relative flex h-screen flex-col justify-center items-center text-white overflow-hidden">
       <div className="absolute top-0 left-0 w-full h-full z-0 pointer-events-none">
+        {/* Image for small screens */}
+        <div className="block md:hidden w-full h-full relative">
+            <Image
+                src="https://firebasestorage.googleapis.com/v0/b/studio-1410114603-9e1f6.firebasestorage.app/o/Site%20Media%2F861edc8f-cf9d-43f9-90b4-a878cd821f99.png?alt=media&token=b35d1330-cdcb-46fe-8313-cfdabadaf3e0"
+                alt="Hero background on mobile"
+                fill
+                className="object-cover"
+            />
+        </div>
+        {/* Video for medium screens and up */}
         <iframe
-          className="absolute top-1/2 left-1/2 min-w-full min-h-full w-auto h-auto -translate-x-1/2 -translate-y-1/2 scale-150"
+          className="hidden md:block absolute top-1/2 left-1/2 min-w-full min-h-full w-auto h-auto -translate-x-1/2 -translate-y-1/2 scale-150"
           src="https://www.youtube.com/embed/sat_OOmdCAo?autoplay=1&mute=1&loop=1&playlist=sat_OOmdCAo&controls=0&showinfo=0&autohide=1&modestbranding=1&iv_load_policy=3&playsinline=1&enablejsapi=1"
           frameBorder="0"
           allow="autoplay; encrypted-media"
